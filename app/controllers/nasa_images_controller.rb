1d1e81c092547a1aa44f77443a4c76d37fb6c1c9
barefoot-yeti <groenie007@gmail.com>
2022-04-20 21:50:15 +0200

    trying to debug heroku

--- app/controllers/nasa_images_controller.rb
+++ app/controllers/nasa_images_controller.rb
@@ -7,6 +7,9 @@ class NasaImagesController < ApplicationController
     city_search_url = "http://api.openweathermap.org/geo/1.0/direct?q=#{city}&appid=#{Rails.application.credentials.weather_api_key}"
     city_result = Faraday.get city_search_url
     
+    puts "DEBUGHERE"
+    puts city_result.body
+    puts JSON.parse(city_result.body).first
     lat = JSON.parse(city_result.body).first['lat'] || "33.918861"
     long = JSON.parse(city_result.body).first['long'] || "18.423300"
     weather_url = "https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{long}&appid=#{Rails.application.credentials.weather_api_key}&units=metric"
